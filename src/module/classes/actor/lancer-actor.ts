f1fd6c38c7d37676ac8e20eda0e9d6fe479e52b4
Eranziel <eranziel@gmail.com>
2020-05-13 03:28:43 +0000

    Create type definitions and classes to match templates

---
+++ src/module/classes/actor/lancer-actor.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Extend the Actor class for Lancer PC-type actors.
+ */
+export class LancerPCActor extends Actor {
+  data: LancerPCActorData;
+
+
+}
+
+
+/**
+ * Extend the Actor class for Lancer NPC-type actors.
+ */
+export class LancerNPCActor extends Actor {
+  data: LancerNPCActorData;
+
+
+}
---
+++ src/module/classes/item/lancer-item.ts
@@ -0,0 +1,28 @@
+
+export class LancerSkill extends Item {
+  data: LancerSkillData;
+}
+
+export class LancerTalent extends Item {
+  data: LancerTalentData;
+}
+
+export class LancerCoreBonus extends Item {
+  data: LancerCoreBonusData;
+}
+
+export class LancerLicense extends Item {
+  data: LancerLicenseData;
+}
+
+export class LancerPilotArmor extends Item {
+  data: LancerPilotArmorData;
+}
+
+export class LancerPilotWeapon extends Item {
+  data: LancerPilotWeaponData;
+}
+
+export class LancerPilotGear extends Item {
+  data: LancerPilotGearData;
+}
