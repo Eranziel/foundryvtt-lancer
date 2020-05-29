a3a06cc32ebabed705ac5380dc65ece07b7031cd
Eranziel <eranziel@gmail.com>
2020-05-29 05:06:07 +0000

    Add template files for missing actor and item types

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,6 +1,14 @@
 export const preloadTemplates = async function() {
 	const templatePaths = [
 		"systems/lancer/templates/actors/pilot.html",
+		"systems/lancer/templates/actors/npc.html",
+		"systems/lancer/templates/actors/deployable.html",
+		"systems/lancer/templates/chat/attack-card.html",
+		"systems/lancer/templates/items/core-bonus.html",
+		"systems/lancer/templates/items/core-license.html",
+		"systems/lancer/templates/items/pilot-gear.html",
+		"systems/lancer/templates/items/skill.html",
+		"systems/lancer/templates/items/talent.html"
 	];
 
 	return loadTemplates(templatePaths);
