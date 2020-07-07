ec018c220d5acc5d723991b24220841df89da0c1
Grygon <647846+Grygon@users.noreply.github.com>
2020-07-06 21:03:01 -0400

    Added frame sheet and better array control

--- src/module/item/item-sheet.ts
+++ src/module/item/item-sheet.ts
@@ -72,6 +72,8 @@ export class LancerItemSheet extends ItemSheet {
     // Yes, theoretically this could be abstracted out to one function. You do it then.
     html.find(".tags-container").on("click", ".clickable", this._onClickTagControl.bind(this));
     html.find(".effects-container").on("click", ".clickable", this._onClickEffectControl.bind(this));
+    html.find(".arrayed-item-container").on("click", ".clickable", this._onClickArrayControl.bind(this));
+    html.find(".arrayed-item-container").on("change", ".delete-selector", this._onSelectDelete.bind(this));
   }
 
   /* -------------------------------------------- */
@@ -82,26 +84,96 @@ export class LancerItemSheet extends ItemSheet {
    * @private
    */
   // TODO: unused currently
-  async _onClickAttributeControl(event) {
+  async _onClickArrayControlNA(event) {
     event.preventDefault();
-    const a = event.currentTarget;
-    const action = a.dataset.action;
-    const tags = this.object.data.data.tags;
-    const form = this.form;
+    const a = $(event.currentTarget);
+    const action = a.data("action");
+    const itemString = a.data("item");
+    const itemArr = duplicate(this["object"]["data"]["data"][itemString]);
+    const dataRef = "data." + itemString;
 
-    // Add new attribute
+    // Add new index
     if ( action === "create" ) {
-      tags.push()
+      // I can't figure out a better way to prevent collisions
+      // Feel free to come up with something better
+      const keys = Object.keys(itemArr);
+      var newIndex = 0;
+      if (keys.length > 0) {
+        newIndex = Math.max.apply(Math, keys) + 1;
+      }
+      itemArr[newIndex] = null;
+      await this.object.update({ dataRef: itemArr });
       await this._onSubmit(event);
-    }
-
-    // Remove existing attribute
-    else if ( action === "delete" ) {
-      const li = a.closest(".tag");
-      li.parentElement.removeChild(li);
-      await this._onSubmit(event);
+    } else if (action === "delete") {
+      const parent = a.parents(".arrayed-item");
+      const id = parent.data("key");
+      delete itemArr[id];
+      itemArr["-=" + id] = null;
+      this.object.update({ dataRef: itemArr });
     }
   }
+
+  async _onSelectDelete(event) {
+    const s = $(event.currentTarget);
+    if(s.val() === "delete") {
+      event.preventDefault();
+      const itemString = s.data("item");
+      var itemArr = duplicate(this["object"]["data"]["data"][itemString]);
+      const parent = s.parents(".arrayed-item");
+      const id = parent.data("key");
+
+      delete itemArr[id];
+      itemArr["-=" + id] = null;
+      console.log(itemArr);
+      await parent.remove();
+      await this.object.update({ "data.mounts" : itemArr });
+
+    }
+
+  }
+
+  async _onClickArrayControl(event) {
+    event.preventDefault();
+    const a = $(event.currentTarget);
+    const action = a.data("action");
+    const itemString = a.data("item");
+    var itemArr = duplicate(getValue(this,("object.data.data." + itemString)));
+    const dataRef = "data." + itemString;
+
+    console.log("_onClickArrayControl()", action, itemArr, itemString);
+    if (action === "create") {
+      // I can't figure out a better way to prevent collisions
+      // Feel free to come up with something better
+      const keys = Object.keys(itemArr);
+      var newIndex = 0;
+      if (keys.length > 0) {
+        newIndex = Math.max.apply(Math, keys) + 1;
+      }
+      itemArr[newIndex] = null;
+      await this.object.update({ [dataRef] : itemArr});
+    } else if (action === "delete") {
+      // delete tag
+      const parent = a.parents(".arrayed-item");
+      const id = parent.data("key");
+      
+      // Since we're doing weird things with arrays, let's just split as needed
+      if (Array.isArray(itemArr)) {
+        // This is for arrays: 
+        itemArr.splice(id,1);
+      } else {
+        // For dict
+        delete itemArr[id];
+        itemArr["-=" + id] = null;
+
+        // So the question now becomes: why do all my arrays become dicts of objects?
+      }
+
+      this.object.update({ [dataRef] : itemArr });
+    }
+  }
+
+  // Yes, abstract it out into a single onClickAddRemove function
+  // You do it then. I'm busy.
 
   async _onClickTagControl(event) {
     event.preventDefault();
@@ -148,7 +220,7 @@ export class LancerItemSheet extends ItemSheet {
       if (keys.length > 0) {
         newIndex = Math.max.apply(Math, keys) + 1;
       }
-      effect[newIndex] = null;
+      effect[newIndex] = {};
       await this.object.update({ "data.effect": effect });
       await this._onSubmit(event);
     } else if (action === "delete") {
@@ -190,4 +262,13 @@ export class LancerItemSheet extends ItemSheet {
     // Update the Item
     return this.object.update(formData);
   }
+}
+
+// Helper function to get arbitrarily deep array references
+function getValue(object, path) {
+    return path.
+        replace(/\[/g, '.').
+        replace(/\]/g, '').
+        split('.').
+        reduce((o, k) => (o || {})[k], object);
 }
