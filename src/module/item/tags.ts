9517e887a1c9681cc4227d5ad1b36b00c3c2aad1
Grygon <647846+Grygon@users.noreply.github.com>
2020-07-10 12:26:39 -0400

    Reworked other tag options

--- src/module/item/tags.ts
+++ src/module/item/tags.ts
@@ -23,60 +23,94 @@ function findTag(id: string): TagData {
 }
 
 /**
- * Handlebars helper to generate compact tag template.
+ * Handlebars helper to generate compact read-only tag template.
  * @param tagShort an object containing the tag's ID and value.
  * @returns The html template for the tag.
  */
-export function renderCompactTag(tagShort: TagDataShort): string {
+export function renderCompactTag(tag: TagData, key: number): string {
   let template: string = "";
-  let tag: TagData = findTag(tagShort.id);
-  
-  if (tag) {
-    // Don't render hidden tags
-    if (tag.hidden) return template;
 
-    // Put the value in the tag string
-    let tagString: string = tag.name;
-    if (tagShort.val) {
-      tagString = tagString.replace("{VAL}", String(tagShort.val));
-    }
-    // Generate the Handlebars partial
-    template = `<div class="compact-tag flexrow">
-    <i class="mdi mdi-label i--s i--light"></i>
-    <span style="margin: 3px;">${tagString}</span>
-    </div>`;
+  // Initialize if we need to
+  if (tag == null) tag = {name: "", description: "", id: ""};
+
+  // Don't render hidden tags
+  if (tag["hidden"]) return template;
+
+  // If we have a pre-defined tag, insert info. Otherwise, leave it as-is.
+  if (tag["id"]) {
+    // Look up values
+    tag = findTag(tag["id"]);
+  } 
+
+  // Put the value in the tag string
+  let tagName: string = tag["name"];
+  let tagDesc: string = tag["description"];
+
+  if (tag["val"]) {
+    tagName = tagName.replace("{VAL}", String(tag["val"]));
+    tagDesc = tagDesc.replace("{VAL}", String(tag["val"]));
   }
+
+  // Generate the Handlebars partial. This will always be the read-only
+  template = `<div class="compact-tag flexrow">
+  <i class="mdi mdi-label i--s i--light"></i>
+  <span style="margin: 3px;">${tagName}</span>
+  </div>`;
+
   return template;
 }
 
-export function renderChunkyTag(tagShort: TagDataShort): string {
+export function renderChunkyTag(tag: TagData, key: number): string {
   let template: string = "";
-  let tag: TagData = findTag(tagShort.id);
-  
-  if (tag) {
-    // Don't render hidden tags
-    if (tag.hidden) return template;
+
+  // Initialize if we need to
+  if (tag == null) tag = {name: "", description: "", id: ""};
+
+  // Don't render hidden tags
+  if (tag["hidden"]) return template;
+
+  // If we have a pre-defined tag, insert info. Otherwise, leave it as-is.
+  if (tag["id"]) {
+    // Look up values
+    tag = findTag(tag["id"]);
 
     // Put the value in the tag string
-    let tagString: string = tag.name;
-    let tagDescription: string = tag.description;
-    if (tagShort.val) {
-      tagString = tagString.replace("{VAL}", String(tagShort.val));
-      tagDescription = tagDescription.replace("{VAL}", String(tagShort.val));
+    let tagName: string = tag["name"];
+    let tagDesc: string = tag["description"];
+
+    if (tag["val"]) {
+      tagName = tagName.replace("{VAL}", String(tag["val"]));
+      tagDesc = tagDesc.replace("{VAL}", String(tag["val"]));
     }
+
     // Generate the Handlebars partial
-    template = `<div class="tag flexrow">
+    template = `<div class="tag flexrow arrayed-item" data-key="${key}">
       <div class="tag-label">
         <i class="med-icon fa fa-3x fa-tag" style="margin: 3px"></i>
       </div>
       <div class="flexcol">
-        <div class="remove-wrapper">
-          <span class="major" style="margin: 3px;">${tagString}</span>
-          <span>${tagDescription}</span>
-        </div>
+          <span class="major" style="margin: 3px;">${tagName}</span>
+          <span>${tagDesc}</span>
+          <a class="remove-button fa fa-trash clickable" data-action="delete" style="grid-area: 2/3/3/4; margin-right: 11px; margin-top: -.8em; justify-self: right; align-self: self-start"></a>
       </div>
     </div>`;
   }
+
+  // Editable partial
+  template = `<div class="tag flexrow arrayed-item" data-key="${key}">
+      <div class="tag-label">
+        <i class="med-icon fa fa-3x fa-tag" style="margin: 3px"></i>
+      </div>
+      <div class="flexcol">
+            <input type="String" name="data.tags.${key}.name" value="${tag["name"]}" data-dtype="String" class="lancer-invisible-input medium theme--main" style="grid-area: 1/2/2/3; text-align:left; padding-left: 0.5em; margin-top: 0.25em;"/>
+          <textarea class="lancer-invisible-input effect-text" type="string" name="data.tags.${key}.description" data-dtype="String" style="grid-area: 2/2/3/3">${tag["description"]}</textarea>
+          <a class="remove-button fa fa-trash clickable" data-action="delete" style="grid-area: 2/3/3/4; margin-right: 11px; margin-top: -.8em; justify-self: right; align-self: self-start"></a>
+      </div>
+    </div>`;
+
+          
+
+  return template;
 }
   
 /**
@@ -95,7 +129,7 @@ export function renderFullTag(tag: TagData, key: number): string {
   if (tag["hidden"]) return template;
 
   // If we have a pre-defined tag, insert info. Otherwise, leave it as-is.
-  if (tag && tag["id"]) {
+  if (tag["id"]) {
     // Look up values
     tag = findTag(tag["id"]);
 
