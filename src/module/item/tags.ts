870cb560cf9ce5de9b40e13803b3cbf82707bd7e
Grygon <647846+Grygon@users.noreply.github.com>
2020-07-10 02:27:41 -0500

    Add tags & array control. Basic frame work.

--- src/module/item/tags.ts
+++ src/module/item/tags.ts
@@ -8,12 +8,15 @@ import { TagData, TagDataShort } from '../interfaces';
  * @returns The full tag data.
  */
 function findTag(id: string): TagData {
-  const tags = data.tags;
-	// Find the tag id in lancer-data
-  for(let i = 0; i < tags.length; i++) {
-    const t = tags[i];
-    if (t.id === id) {
-      return t;
+  // Only check if we actually got something.
+  if(id) {
+    const tags = data.tags;
+  	// Find the tag id in lancer-data
+    for(let i = 0; i < tags.length; i++) {
+      const t = tags[i];
+      if (t.id === id) {
+        return t;
+      }
     }
   }
   return null;
@@ -81,28 +84,49 @@ export function renderChunkyTag(tagShort: TagDataShort): string {
  * @param tagShort an object containing the tag's ID and value.
  * @returns The html template for the tag.
  */
-export function renderFullTag(tagShort: TagDataShort): string {
-  let template: string = "";
-  let tag: TagData = findTag(tagShort.id);
+export function renderFullTag(tag: TagData, key: number): string {
 
-  if (tag) {
-    // Don't render hidden tags
-    if (tag.hidden) return template;
+  let template: string = "";
+
+  // Initialize if we need to
+  if (tag == null) tag = {name: "", description: "", id: ""};
+
+  // Don't render hidden tags
+  if (tag["hidden"]) return template;
+
+  // If we have a pre-defined tag, insert info. Otherwise, leave it as-is.
+  if (tag && tag["id"]) {
+    // Look up values
+    tag = findTag(tag["id"]);
 
     // Put the value in the tag string
-    let tagName: string = tag.name;
-    let tagDesc: string = tag.description;
-    if (tagShort.val) {
-      tagName = tagName.replace("{VAL}", String(tagShort.val));
-      tagDesc = tagDesc.replace("{VAL}", String(tagShort.val));
+    let tagName: string = tag["name"];
+    let tagDesc: string = tag["description"];
+
+    if (tag["val"]) {
+      tagName = tagName.replace("{VAL}", String(tag["val"]));
+      tagDesc = tagDesc.replace("{VAL}", String(tag["val"]));
     }
-    // Generate the Handlebars partial
-    template = `<div class="tag">
+
+    // Read-only partial
+    template = `<div class="tag arrayed-item" data-key="${key}">
     <i class="mdi mdi-label i--l theme--main" style="grid-area: 1/1/3/2;"></i>
-    <div class="medium theme--main" style="grid-area: 1/2/2/3; text-align:left; padding-left: 0.5em;">${tagName}</div>
+    <div class="medium theme--main" style="grid-area: 1/2/2/3; text-align:left; padding-left: 0.5em; margin-top: 0.25em;">${tagName}</div>
     <div class="effect-text" style="grid-area: 2/2/3/3">${tagDesc}</div>
-    <a class="remove-button fa fa-trash clickable" style="grid-area: 2/3/3/4; margin-right: 11px; margin-top: 2px"></a>
+    <a class="remove-button fa fa-trash clickable" data-action="delete" style="grid-area: 2/3/3/4; margin-right: 11px; margin-top: -.8em; justify-self: right; align-self: self-start"></a>
     </div>`;
-  }
+    return template;
+  } 
+
+  // Editable partial
+  template = `<div class="tag arrayed-item" data-key="${key}">
+            <i class="mdi mdi-label i--l theme--main" style="grid-area: 1/1/3/2;"></i>
+            <input type="String" name="data.tags.${key}.name" value="${tag["name"]}" data-dtype="String" class="lancer-invisible-input medium theme--main" style="grid-area: 1/2/2/3; text-align:left; padding-left: 0.5em; margin-top: 0.25em;"/>
+          <textarea class="lancer-invisible-input effect-text" type="string" name="data.tags.${key}.description" data-dtype="String" style="grid-area: 2/2/3/3">${tag["description"]}</textarea>
+            <a class="remove-button fa fa-trash clickable" data-action="delete" style="grid-area: 2/3/3/4; margin-right: 11px; margin-top: -.8em; justify-self: right; align-self: self-start"></a>
+          </div>`;
+
+          
+
   return template;
 }
