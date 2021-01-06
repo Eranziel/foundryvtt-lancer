b22d6f71313ec46c1e04cd0935e2ef0730f884a5
Scott Mundorff <scott.mundorff@gmail.com>
2021-01-06 16:28:12 -0500

    don't need to keep highest for every roll

--- src/module/macros.ts
+++ src/module/macros.ts
@@ -422,7 +422,7 @@ async function rollAttackMacro(actor: Actor, data: LancerAttackMacroData) {
         // add die explosion on 1 and keep the highest of the original number of die
         if(data.overkill) droll.dice[die].modifiers.push("x1");
         // for both sections above, we want to keep the highest of the die count
-        droll.dice[die].modifiers.push(`kh${die_count}`);
+        if(attack_roll.total >= 20 || data.overkill) droll.dice[die].modifiers.push(`kh${die_count}`);
       }
       
       droll = droll.roll();
