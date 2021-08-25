f4d16862283c688ff237ed372ece79aac9c427af
Sohum Banerjea <sohumb@gmail.com>
2021-08-25 10:49:44 -0700

    clean up svelte-application, provide useful promise methods, support intro: true

--- src/module/helpers/svelte-application.ts
+++ src/module/helpers/svelte-application.ts
@@ -1,46 +1,66 @@
 import type SvelteComponent from '*.svelte';
 
+type SvelteAppOptions = ApplicationOptions & {
+  intro?: boolean
+}
+
 export default class SvelteApp<DataModel> extends Application {
   klass: typeof SvelteComponent;
   data: DataModel;
-  component!: typeof SvelteComponent; // the type reuses the same type for class and instance
+  component?: typeof SvelteComponent; // the type reuses the same type for class and instance
+  declare options: SvelteAppOptions;
 
   #resolve: ((data: DataModel) => void) | null = null;
   #reject: ((v: void) => void) | null = null;
-  promise: Promise<DataModel>;
+  promise!: Promise<DataModel>; // constructor calls refreshPromise(), which definitely assigns this
 
-  constructor(App: typeof SvelteComponent, data: DataModel, options: ApplicationOptions) {
+  constructor(App: typeof SvelteComponent, data: DataModel, options?: SvelteAppOptions) {
     super(options);
-    this.promise = new Promise((resolve, reject) => {
-      this.#resolve = resolve;
-      this.#reject = reject;
-    })
+    this.refreshPromise();
     this.data = data;
     this.klass = App;
   }
 
-  activateListeners(html: JQuery) {
-    this.component = new this.klass({
-      target: html.get(0),
-      props: this.data,
-    });
-    this.component.$on('submit', (_e: Event) => {
-      if (this.#resolve) {
-        this.#resolve(this.data);
-        this.#reject = null;
-      }
-      return this.close();
-    });
-    this.component.$on('cancel', (_e: Event) => {
-      return this.close();
-    });
+  refreshPromise() {
+    if (this.#reject) { this.#reject(); }
+    this.promise = new Promise((resolve, reject) => {
+      this.#resolve = resolve;
+      this.#reject = reject;
+    })
   }
 
-  close() {
+  resolvePromise() {
+    if (this.#resolve) {
+      this.#resolve(this.data);
+      this.#reject = null;
+    }
+  }
+
+  rejectPromise() {
     if (this.#reject) {
       this.#reject();
       this.#resolve = null;
     }
+  }
+
+  activateListeners(html: JQuery) {
+    let component = new this.klass({
+      target: html.get(0),
+      props: this.data,
+      intro: !!this.options.intro
+    });
+    component.$on('submit', (_e: Event) => {
+      this.resolvePromise();
+      return this.close();
+    });
+    component.$on('cancel', (_e: Event) => {
+      return this.close();
+    });
+    this.component = component;
+  }
+
+  close() {
+    this.rejectPromise()
     return super.close();
   }
 
