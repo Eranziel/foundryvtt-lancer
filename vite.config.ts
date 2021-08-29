5fae4d033494f7e98ef8c5a7b4dd6ff6a28da204
Sohum Banerjea <sohumb@gmail.com>
2021-08-29 11:56:50 -0700

    machine mind update introduces a new cjs dependency

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ const config: UserConfig = {
   },
   optimizeDeps: {
     exclude: ["machine-mind"], // machine-mind triggers https://github.com/evanw/esbuild/issues/1433
-    include: ["lancer-data", "jszip", "axios"], // machine-mind's cjs dependencies
+    include: ["lancer-data", "jszip", "axios", "readonly-proxy"], // machine-mind's cjs dependencies
   },
   build: {
     outDir: path.resolve(__dirname, "dist"),
