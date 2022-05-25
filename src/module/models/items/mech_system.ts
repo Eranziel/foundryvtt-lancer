550ebd473381ef920ec3b3d6426e19b3322c4a49
whitespine <jakehenry.tn@gmail.com>
2022-05-25 01:15:35 -0400

    Mech system model

--- src/module/models/items/mech_system.ts
+++ src/module/models/items/mech_system.ts
@@ -1,31 +1,24 @@
 // @ts-nocheck
 
-import { ActionsField } from "../bits/action";
+import { template_universal_item } from "./shared";
 import { template_bascdt } from "./shared.ts";
-
-class MyItemModel extends foundry.abstract.DataModel {
-  static defineSchema() {
-    return {
-      stats: // ...
-    };
-  }
-}
 
 const fields = foundry.data.fields;
 
 export class MechSystemModel extends foundry.abstract.DataModel {
   static defineSchema() {
-    const key = new fields.StringField({ required: true, blank: false, nullable: false, label: 'Key' });
-    const value = new fields.NumberField({ required: true, nullable: false, label: 'Val' });
-    const keyValuePairDataSchema = { key, value };
-    const keyValuePairSchemaField = new fields.SchemaField(keyValuePairDataSchema);
-
     return {
       cascading: new fields.BooleanField(),
       destroyed: new fields.BooleanField(),
       effect: new fields.HTMLField(),
-      license: new fields.StringField({ trim: 
-      ...template_bascdt(),
+      license: new fields.StringField(),
+      source: new fields.StringField({ nullable: true }),
+      license_level: new fields.NumberField({ nullable: false, initial: 0 }),
+      sp: new fields.NumberField({ nullable: false, initial: 0 }),
+      uses: new fields.NumberField({ nullable: false, initial: 0 }),
+      description: new fields.HTMLField(),
+      type: new fields.StringField(),
+      ...template_universal_item(),
       ...template_bascdt(),
     };
   }
