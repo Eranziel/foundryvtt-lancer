2ae6c57473e0293f98d7381b1e80bf05a2e5e82f
whitespine <jakehenry.tn@gmail.com>
2022-05-25 01:37:04 -0400

    Counter no longer uses a counter object

--- src/module/models/bits/counter.ts
+++ src/module/models/bits/counter.ts
@@ -1,10 +1,10 @@
-// @ts-nocheck
-const fields = foundry.data.fields;
-
 import { RegCounterData } from "machine-mind";
 import { LIDField } from "../shared";
 
-// A single <type, value> pairing for damage. Mimics RegCounterData
+// @ts-ignore
+const fields: any = foundry.data.fields;
+
+// A single <type, value> pairing for damage. mimics RegCounterData
 export class CounterField extends fields.SchemaField {
   constructor(options = {}) {
     super(
@@ -20,18 +20,24 @@ export class CounterField extends fields.SchemaField {
     );
   }
 
-  /** @override */
-  initialize(model, name, value: RegCounterData) {
-    // Coerce to a range
-    return new Counter(value);
+  bound_val(value: RegCounterData, sub_val: number) {
+    sub_val = Math.round(sub_val);
+    sub_val = Math.max(sub_val, value.min);
+    if ( value.max !== null ) sub_val = Math.min(sub_val, value.max);
+    return sub_val;
+  }
+
+  /** @inheritdoc */
+  clean(value: RegCounterData, data: any, options: any) {
+    // Attempt to move our .val back in bounds
+    value = super.clean(value, data, options);
+    value.val = this.bound_val(value, value.val || 0);
+    value.default_value = this.bound_val(value, value.default_value || 0);
+    return value;
   }
 
   /** @override */
-  _cast(value) {
-    if( value instanceof Counter ) {
-      return value.save();
-    } else {
-      return super._cast(value);
-    }
+  _validateType(value: RegCounterData) {
+    if ( value.max !== null && (value.max < value.min) ) throw new Error("max must be > min");
   }
 }
