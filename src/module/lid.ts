e8d584b7dc6747f03fa2184424b2f27ab23b2b5f
whitespine <jakehenry.tn@gmail.com>
2022-06-04 16:10:43 -0400

    Made non-MM lid lookup utility

---
+++ src/module/lid.ts
@@ -0,0 +1,45 @@
+import { EntryType } from "machine-mind";
+import { LancerActor } from "./actor/lancer-actor";
+import { LancerItem } from "./item/lancer-item";
+
+
+// Mechanisms for LID resolution
+
+/**
+ * Lookup all documents with the associated lid in the given types.
+ * Document types are checked in order. If no type(s) supplied, all are queried.
+ * short_circuit will make it stop with first valid result. Will still return all results of that category
+ */
+export async function compendium_lookup_lid_plural(lid: string, short_circuit: boolean=true, types?: EntryType | EntryType[]): Promise<Array<LancerActor | LancerItem>>{
+    // Note: typeless lookup is (somewhat obviously) up to 13x more expensive than non
+    if(!types) {
+        types = Object.values(EntryType); 
+    } else if(!Array.isArray(types)) {
+        types = [types];
+    } 
+
+    let result: Array<LancerActor | LancerItem> = [];
+    for(let t of types) {
+        let pack = game.packs.get(`world.${t}`)!;
+        await pack?.getDocuments({lid: lid}).then(docs => {
+            if(docs.length) {
+                if(short_circuit) {
+                    return docs;
+                } else {
+                    result.push(...docs);
+                }
+            }
+        });
+    }
+    return result;
+}
+
+// As compendium_lookup_lid, but just takes first result
+export async function compendium_lookup_lid(lid: string, types?: EntryType | EntryType[]): Promise<LancerActor | LancerItem | null> {
+    let res = await compendium_lookup_lid_plural(lid, true, types);
+    if(res.length) {
+        return res[0];
+    } else {
+        return null;
+    }
+}
--- src/module/models/bits/tag.ts
+++ src/module/models/bits/tag.ts
@@ -1,3 +1,5 @@
+import { EntryType } from "machine-mind";
+import { compendium_lookup_lid } from "../../lid";
 import { LIDField } from "../shared";
 
 // @ts-ignore
@@ -6,17 +8,15 @@ const fields: any = foundry.data.fields;
 const PLACEHOLDER = "...";
 
 export interface TagFieldSourceData {
-  lid: string,
-  val: string
+  lid: string;
+  val: string;
 }
 
 export interface TagFieldSystemData extends TagFieldSourceData {
   name: string;
-  description: string,
-  num_val: number | null
+  description: string;
+  num_val: number | null;
 }
-
-
 
 // A single <lid, value> pairing for tags. Caches its name for quicker lookup
 export class TagField extends fields.SchemaField {
@@ -24,7 +24,7 @@ export class TagField extends fields.SchemaField {
     super(
       {
         lid: new LIDField(),
-        val: new fields.StringField({nullable: false}),
+        val: new fields.StringField({ nullable: false }),
       },
       options
     );
@@ -36,9 +36,9 @@ export class TagField extends fields.SchemaField {
     let parsed = Number.parseInt(value.val);
     let num_val: number | null;
     if (Number.isNaN(parsed)) {
-        num_val = null;
+      num_val = null;
     } else {
-        num_val = parsed;
+      num_val = parsed;
     }
 
     // Turn it into our expanded format
@@ -46,20 +46,18 @@ export class TagField extends fields.SchemaField {
       ...foundry.utils.duplicate(value),
       name: PLACEHOLDER,
       description: PLACEHOLDER,
-      num_val
-    }
+      num_val,
+    };
 
     // Begin a job to look it up from the actual tag object
-    let tag_pack = game.packs.get("world.tag")!;
-    tag_pack?.getDocuments({lid: value.lid}).then(docs => {
-        if(docs.length) {
-            let doc = docs[0];
-            rv["name"] = doc.name;
-            rv["description"] = doc.system.description;
-        } else {
-            rv["name"] = "MISSINGTAG";
-            rv["description"] = `Tag "${value.lid}" was not found in your tag compendium`;
-        }
+    compendium_lookup_lid(value.lid, EntryType.TAG).then(doc => {
+      if (doc) {
+        rv["name"] = doc.name;
+        rv["description"] = doc.system.description;
+      } else {
+        rv["name"] = "MISSINGTAG";
+        rv["description"] = `Tag "${value.lid}" was not found in your tag compendium`;
+      }
     });
 
     // That's it
@@ -68,10 +66,10 @@ export class TagField extends fields.SchemaField {
 
   /** @override */
   _cast(value: any) {
-      // Make sure num_val gets turned back into val
-      if(value.num_val) {
-        value["val"] = String(value.num_val);
-      }
-      return super._cast(value);
+    // Make sure num_val gets turned back into val
+    if (value.num_val) {
+      value["val"] = String(value.num_val);
+    }
+    return super._cast(value);
   }
 }
