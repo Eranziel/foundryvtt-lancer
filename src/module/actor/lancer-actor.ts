ccf7e361b3a880bddacad7b8e851126f768dcfe8
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2022-06-17 15:47:07 -0500

    Fix wording on destabilized power plant.
    The description incorrectly listed electric instead of energy.
    
    Fixes #434

--- src/module/actor/lancer-actor.ts
+++ src/module/actor/lancer-actor.ts
@@ -164,7 +164,7 @@ export class LancerActor extends Actor {
         case 2:
         case 3:
         case 4:
-          return "The power plant becomes unstable, beginning to eject jets of plasma. Your mech becomes @Compendium[world.status.EXPOSED], taking double kinetic, explosive and electric damage until the status is cleared.";
+          return "The power plant becomes unstable, beginning to eject jets of plasma. Your mech becomes @Compendium[world.status.EXPOSED], taking double kinetic, explosive and energy damage until the status is cleared.";
         case 5:
         case 6:
           return "Your mech’s cooling systems manage to contain the increasing heat; however, your mech becomes @Compendium[world.status.IMPAIRED] until the end of your next turn.";
