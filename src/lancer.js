b591d9bfcf65b65d1820f229c6a25596dc9929b1
WHITESPINE <jakehenry.tn@gmail.com>
2023-03-02 22:40:26 +0000

    Bulding, mostly working

--- src/lancer.js
+++ src/lancer.js
@@ -1,6 +1,7 @@
-// only required for dev
+// only required for dev - test 1
 // in prod, foundry loads index.js, which is compiled by vite/rollup
 // in dev, foundry loads index.js, this file, which loads lancer.ts
 
 window.global = window;
 import * as LANCER from "./lancer.ts";
+console.warn("WECOME TO HELL");
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,42 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
 import { visualizer } from "rollup-plugin-visualizer";
+import resolve from "@rollup/plugin-node-resolve"; // This resolves NPM modules from node_modules.
+// @ts-ignore
+import { postcssConfig, terserConfig, typhonjsRuntime } from "@typhonjs-fvtt/runtime/rollup";
+
+const s_COMPRESS = false; // Set to true to compress the module bundle.
+const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
+
+// Set to true to enable linking against the TyphonJS Runtime Library module.
+// You must add a Foundry module dependency on the `typhonjs` Foundry package or manually install it in Foundry from:
+// https://github.com/typhonjs-fvtt-lib/typhonjs/releases/latest/download/module.json
+const s_TYPHONJS_MODULE_LIB = false;
+
 // import preprocess from "svelte-preprocess";
 // const path = require("path");
 import path from "path";
 
 const config = defineConfig({
   root: "src/",
-  base: "/systems/lancer/",
+  base: "/systems/lancer",
   publicDir: path.resolve(__dirname, "public"),
+  cacheDir: "../.vite-cache", // Relative from root directory.
+
+  esbuild: {
+    target: ["es2022", "chrome100"],
+    keepNames: true, // Note: doesn't seem to work.
+  },
+
+  css: {
+    // Creates a standard configuration for PostCSS with autoprefixer & postcss-preset-env.
+    postcss: postcssConfig({ compress: s_COMPRESS, sourceMap: s_SOURCEMAPS }),
+  },
+
   server: {
     port: 30001,
-    open: true,
+    open: "/game",
     proxy: {
       "^(?!/systems/lancer)": "http://localhost:30000/",
       "/socket.io": {
@@ -21,6 +46,7 @@ const config = defineConfig({
     },
   },
   resolve: {
+    conditions: ["import", "browser"],
     alias: [
       {
         find: "./runtimeConfig",
@@ -35,6 +61,7 @@ const config = defineConfig({
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
     sourcemap: true,
+    minify: s_COMPRESS ? "terser" : false,
     terserOptions: {
       mangle: false,
       keep_classnames: true,
@@ -42,13 +69,14 @@ const config = defineConfig({
     },
     lib: {
       name: "lancer",
-      entry: path.resolve(__dirname, "src/lancer.ts"),
+      entry: path.resolve(__dirname, "src/lancer.js"),
       formats: ["es"],
       fileName: "lancer",
     },
   },
   plugins: [
     svelte({
+      preprocess: preprocess(),
       // preprocess: preprocess(),
       configFile: "../svelte.config.js", // relative to src/
     }),
@@ -58,10 +86,17 @@ const config = defineConfig({
       // svelte: { root: __dirname },
     }),
     */
-    visualizer({
-      gzipSize: true,
-      template: "treemap",
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
     }),
+
+    s_TYPHONJS_MODULE_LIB && typhonjsRuntime(),
+
+    // visualizer({
+    // gzipSize: true,
+    // template: "treemap",
+    // }),
   ],
 });
 
