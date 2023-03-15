1c993ea6ae8507d7253760fcad2f8a1235a01a94
Eranziel <eranziel@gmail.com>
2023-03-14 23:17:17 -0600

    Start custom styling for hp & heat bars

--- src/module/apps/components/StatBar.svelte
+++ src/module/apps/components/StatBar.svelte
@@ -4,28 +4,32 @@
   import { LancerActor } from '../../actor/lancer-actor.js';
   import { resolve_dotpath } from '../../helpers/commons.js';
   import LinearProgress from '@smui/linear-progress';
+  import { includes } from 'fp-ts/lib/string.js';
+  import "./stat-bar.scss";
 
   const actor: Readable<LancerActor> = getContext("actor");
   
   // Props
   export let path: string;
   export let icon: string;
-  
+
   let stat: { min: number, max: number, value: number };
   let progress: number;
+  let hp: boolean;
+  let heat: boolean;
+  
   $: stat = resolve_dotpath($actor, path) ?? {min: 0, max: 1, value: 0};
   $: progress = (stat.value) / (stat.max || 1);
+  $: hp = path.includes("hp");
+  $: heat = path.includes("heat");
 </script>
 
 <div class="lancer-stat-bar flexrow">
   <i class="{icon} i--s"></i>
-  <LinearProgress {progress} />
+  <LinearProgress {progress} class={hp ? "hp-bar" : (heat ? "heat-bar" : "")} />
 </div>
 
 <style lang="scss">
-  @use '@material/linear-progress/index' as linear-progress;
-  @use '@material/theme/index' as theme;
-
   .lancer-stat-bar {
     text-align: left;
     align-items: center;
@@ -35,5 +39,4 @@
     }
   }
 
-  
 </style>
