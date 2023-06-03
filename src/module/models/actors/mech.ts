6c519e6f89847455a3bbe84fef0681b245d5d31f
WHITESPINE <jakehenry.tn@gmail.com>
2023-06-03 19:03:28 +0000

    Mech loadout migration

--- src/module/models/actors/mech.ts
+++ src/module/models/actors/mech.ts
@@ -8,7 +8,7 @@ import {
 
 import { FakeBoundedNumberField, LancerDataModel, EmbeddedRefField, SyncUUIDRefField } from "../shared";
 import { EntryType, FittingSize, MountType } from "../../enums";
-import { regRefToUuid } from "../../migration";
+import { regRefToId, regRefToUuid } from "../../migration";
 
 const fields: any = foundry.data.fields;
 
@@ -53,6 +53,34 @@ export class MechModel extends LancerDataModel<"MechModel"> {
     // Convert old regrefs
     if (typeof data.pilot == "object") {
       data.pilot = regRefToUuid("Actor", data.pilot);
+    }
+
+    // Convert loadout
+    // Always try to convert frame away from regref
+    if (data.loadout?.frame) {
+      data.loadout.frame = regRefToId("Item", data.loadout.frame);
+    }
+    // If we don't already have a systems array attempt to convert a system_mounts array
+    if (Array.isArray(data.loadout?.system_mounts)) {
+      // Remap the var name + convert from regref + remove nulls
+      data.loadout.systems ??= data.loadout.system_mounts
+        .map((sm: any) => regRefToId("Item", sm?.system))
+        .filter((sm: string | null) => sm);
+    }
+    if (Array.isArray(data.loadout?.weapon_mounts)) {
+      // First remove nulls
+      data.loadout.weapon_mounts = data.loadout.weapon_mounts.filter((wm: any) => wm); // Remove nulls if they exist
+      // Then process mount-by-mount
+      for (let wm of data.loadout.weapon_mounts) {
+        wm.type ??= wm?.mount_type; // type got renamed
+        wm.slots = wm.slots.filter((s: any) => s); // Remove nulls if they exist
+        for (let slot of wm.slots) {
+          // Fix slotwise. in particular just need to fix weapon and mod
+          slot.weapon = regRefToId("Item", slot.weapon);
+          slot.mod = regRefToId("Item", slot.mod);
+        }
+      }
+      data.loadout.weapon_mounts = data.loadout.system_mounts.map((sm: any) => regRefToId("Item", sm?.system));
     }
 
     // @ts-expect-error v11
