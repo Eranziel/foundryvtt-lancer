a1c81f8bc319dedca4003b4fe1ce9be2f87409d1
Eranziel <eranziel@gmail.com>
2023-06-04 21:37:51 -0600

    Prevent error preparing data for new mech

--- src/module/actor/lancer-actor.ts
+++ src/module/actor/lancer-actor.ts
@@ -348,6 +348,8 @@ export class LancerActor extends Actor {
       // Collect all bonuses
       // TODO - eventually we would rather have these handled via active effects
       this.system.all_bonuses = [];
+      // Ensure loadout helper is initialized.
+      if (!this.loadoutHelper) this.loadoutHelper = new LoadoutHelper(this);
       for (let item of this.loadoutHelper.listLoadout()) {
         this.system.all_bonuses.push(...(item.getBonuses() ?? []));
       }
