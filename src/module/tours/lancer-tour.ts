8ecadf31c69d622066ab61c24b609f2b0ca75f38
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2024-03-24 14:57:58 -0500

    Add Combat tracker tour

--- src/module/tours/lancer-tour.ts
+++ src/module/tours/lancer-tour.ts
@@ -1,22 +1,47 @@
-// @ts-nocheck Tours aren't typed yet
+declare global {
+  interface Game {
+    tours: any;
+  }
+  class Tour {
+    static fromJSON(json_file: string): Promise<Tour>;
+    currentStep: TourStep;
+    _preStep(): Promise<void>;
+    _postStep(): Promise<void>;
+  }
+}
+
+interface TourStep {
+  id: string;
+  selector: string;
+  title: string;
+  content: string;
+  click?: boolean;
+  sidebarTab?: string;
+  inApp?: boolean;
+}
 
 import { LCPManager } from "../apps/lcp-manager";
+import { LancerCombat } from "../combat/lancer-combat";
 
 export class LancerTour extends Tour {
   async _preStep() {
     await super._preStep();
-    if (this.currentStep?.click) {
-      document.querySelector(this.currentStep.selector).click();
-    }
     if (this.currentStep?.sidebarTab) {
       ui.sidebar?.expand();
       ui.sidebar?.activateTab(this.currentStep.sidebarTab);
     }
   }
+
+  async _postStep() {
+    await super._postStep();
+    if (this.currentStep?.click) {
+      document.querySelector<HTMLElement>(this.currentStep.selector)?.click();
+    }
+  }
 }
 
 export class LancerLcpTour extends LancerTour {
-  manager: LCPManager | undefined;
+  manager?: LCPManager;
   async _preStep() {
     await super._preStep();
     if (!this.manager) this.manager = new LCPManager();
@@ -36,8 +61,76 @@ export class LancerLcpTour extends LancerTour {
         setTimeout(resolve, 30);
       });
     }
-    if (this.currentStep?.inApp) {
+    if (this.currentStep.inApp) {
+      // @ts-expect-error Incorrectly labeled protected
       await this.manager._render(true);
     }
   }
 }
+
+export class LancerCombatTour extends LancerTour {
+  combat?: LancerCombat;
+  async _preStep() {
+    await super._preStep();
+    if (!this.combat) this.combat = (await this._setupCombat())!;
+    await this.combat.activate();
+    if (!this.combat.started) await this.combat.startCombat();
+  }
+
+  protected async _setupCombat() {
+    return Combat.create({
+      [`flags.${game.system.id}.tour`]: true,
+      combatants: [
+        ...get_player_data(),
+        {
+          name: "Assault (1)",
+          img: `./systems/${game.system.id}/assets/retrograde-minis/Retrograde-Minis-Corpro-ASSAULT.png` as const,
+          [`flags.${game.system.id}.tour` as const]: true,
+          [`flags.${game.system.id}.disposition` as const]: -1,
+        },
+        {
+          name: "Assault (2)",
+          img: `./systems/${game.system.id}/assets/retrograde-minis/Retrograde-Minis-Corpro-ASSAULT.png` as const,
+          [`flags.${game.system.id}.tour` as const]: true,
+          [`flags.${game.system.id}.disposition` as const]: -1,
+        },
+        {
+          name: "Support (1)",
+          img: `./systems/${game.system.id}/assets/retrograde-minis/Retrograde-Minis-Corpro-SUPPORT.png` as const,
+          [`flags.${game.system.id}.tour` as const]: true,
+          [`flags.${game.system.id}.disposition` as const]: -1,
+        },
+      ].map(foundry.utils.expandObject),
+    });
+  }
+}
+
+function get_player_data() {
+  // Fisher-Yates
+  for (let i = demo_pilots.length - 1; i > 0; --i) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [demo_pilots[i], demo_pilots[j]] = [demo_pilots[j], demo_pilots[i]];
+  }
+  return demo_pilots.slice(0, 3).map(p => ({
+    name: p.name,
+    img: `./systems/${game.system.id}/assets/retrograde-minis/Retrograde-Minis-${p.img}.png` as const,
+    [`flags.${game.system.id}.tour` as const]: true,
+    [`flags.${game.system.id}.disposition` as const]: 2,
+  }));
+}
+// Data subitted from the lancer-vtt channel in Pilot NET
+const demo_pilots = [
+  { name: "Catastrophe", img: "Horus-MANTICORE" }, // Rais
+  { name: "Closing Crescendo", img: "IPS-N-RALEIGH" }, // traduiz
+  { name: "Errant", img: "IPS-N-NELSON" }, // FactualInsanity
+  { name: "Instrument", img: "SSC-MONARCH" }, // Jazzman - Update to viceroy if we add an icon
+  { name: "Owl", img: "SSC-DEATHS HEAD" }, // Jimothy
+  { name: "Oxhorn", img: "HA-SHERMAN" }, // Bargo
+  { name: "Raven", img: "SSC-DUSK WING" }, // Zenn
+  { name: "Sunset", img: "SSC-MOURNING CLOAK" }, // Zemyla
+  { name: "Timber", img: "IPS-N-BLACKBEARD" }, // Valkyrion
+  { name: "Tyrant", img: "Horus-BALOR" }, // Night Parade in the Kitchen
+  { name: "Verminspeaker", img: "Horus-HYDRA" }, // Cancermantis
+  { name: "jellyfish", img: "SSC-ATLAS" }, // Marfew
+  // { name: "Callsign", img: "GMS" },
+];
--- src/module/tours/register-tours.ts
+++ src/module/tours/register-tours.ts
@@ -1,6 +1,6 @@
-import { LancerLcpTour } from "./lancer-tour";
+import { LancerCombatTour, LancerLcpTour } from "./lancer-tour";
 
 export async function registerTours() {
-  // @ts-expect-error Tours not typed
   game.tours.register(game.system.id, "lcp", await LancerLcpTour.fromJSON("./systems/lancer/tours/lcp.json"));
+  game.tours.register(game.system.id, "combat", await LancerCombatTour.fromJSON("./systems/lancer/tours/combat.json"));
 }
