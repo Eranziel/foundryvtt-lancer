1be5f6d99ac1176c41f89b4366de0e77b2507ff5
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2024-07-10 18:59:21 -0500

    Fix getOccupiedSpaces for v12

--- src/module/token.ts
+++ src/module/token.ts
@@ -48,18 +48,16 @@ function cubesBySize({
         .map(c => {
           if (!alt && !columns) return c;
           if (!alt && columns) return { q: c.r, r: c.s, s: c.q };
-          return { q: -c.s, r: -c.q, s: -c.r };
+          if (alt && columns) return { q: -c.r, r: -c.s, s: -c.q };
+          return { q: -c.q, r: -c.r, s: -c.s };
         })
     );
   }
 }
 
 function altOrientation(token: LancerToken): boolean {
-  // @ts-expect-error
-  const HSSisAltOrientation = game.modules.get("hex-size-support")?.api?.isAltOrientation;
-  // @ts-expect-error
-  if (!HSSisAltOrientation) return token.document.width === 2;
-  return HSSisAltOrientation(token);
+  // @ts-expect-error v12
+  return (token.document.hexagonalShape & 1) === 1;
 }
 
 /**
@@ -114,6 +112,26 @@ export class LancerTokenDocument extends TokenDocument {
 }
 
 /**
+ * Get a basis space for the token. For odd, the center, for even, a predicable space with the center as a vertex
+ */
+function getBasis(token: LancerToken) {
+  // @ts-expect-error v12
+  const symmetrical = token.document.width === token.document.height;
+  // @ts-expect-error v12
+  const even = symmetrical && token.document.width % 2 == 0;
+  if (!symmetrical || !even) return token.center;
+  // @ts-expect-error v12
+  const col: boolean = canvas.grid.columns;
+  const alt = altOrientation(token);
+  const pt = { ...token.center };
+  // @ts-expect-error v12
+  if (col) pt.x = pt.x + ((alt ? -1 : 1) * canvas.grid.sizeX) / 2;
+  // @ts-expect-error v12
+  else pt.y = pt.y + ((alt ? -1 : 1) * canvas.grid.sizeY) / 2;
+  return pt;
+}
+
+/**
  * Extend the base Token class to implement additional system-specific logic.
  * @extends {Token}
  */
@@ -145,34 +163,60 @@ export class LancerToken extends Token {
     }
 
     if (this._spaces.spaces.length === 0) {
-      if (canvas.grid?.isHex) {
-        // @ts-expect-error
-        const cube_space = HexagonalGrid.offsetToCube(
-          // @ts-expect-error
-          HexagonalGrid.pixelsToOffset(this.center, canvas.grid.grid.options),
-          // @ts-expect-error
-          canvas.grid.grid.options
-        );
+      // @ts-expect-error v12
+      if (canvas.grid?.isHexagonal) {
+        // const alt = altOrientation(this);
+        // // @ts-expect-error v12
+        // const symmetrical = this.document.width !== this.document.height;
+        // // @ts-expect-error v12
+        // const even = symmetrical && this.document.width % 2 === 0;
+
+        const basis = getBasis(this);
+        // @ts-expect-error v12
+        const base_cube = canvas.grid.pointToCube(basis);
         const cubes = cubesBySize({
           // @ts-expect-error
           size: this.document.width,
           alt: altOrientation(this),
-          columns: canvas.grid!.grid!.options.columns!,
+          // @ts-expect-error
+          columns: canvas.grid!.columns,
         }).map(c => ({
-          q: c.q + cube_space.q,
-          r: c.r + cube_space.r,
-          s: c.s + cube_space.s,
+          q: c.q + base_cube.q,
+          r: c.r + base_cube.r,
+          s: c.s + base_cube.s,
         }));
         this._spaces.spaces = cubes.map(c => {
-          // @ts-expect-error
-          const p = HexagonalGrid.offsetToPixels(
-            // @ts-expect-error
-            HexagonalGrid.cubeToOffset(c, canvas.grid.grid.options),
-            // @ts-expect-error
-            canvas.grid.grid.options
-          );
-          return { x: p.x + Math.floor(canvas.grid!.grid!.w / 2), y: p.y + Math.floor(canvas.grid!.grid!.h / 2) };
+          // @ts-expect-error v12
+          return canvas.grid!.cubeToPoint(c);
         });
+
+        // // @ts-expect-error
+        // const cube_space = HexagonalGrid.offsetToCube(
+        //   // @ts-expect-error
+        //   HexagonalGrid.pixelsToOffset(this.center, canvas.grid.grid.options),
+        //   // @ts-expect-error
+        //   canvas.grid.grid.options
+        // );
+        // const cubes = cubesbysize({
+        //   // @ts-expect-error
+        //   size: this.document.width,
+        //   alt: altorientation(this),
+        //   columns: canvas.grid!.grid!.options.columns!,
+        // }).map(c => ({
+        //   q: c.q + cube_space.q,
+        //   r: c.r + cube_space.r,
+        //   s: c.s + cube_space.s,
+        // }));
+        // this._spaces.spaces = cubes.map(c => {
+        //   // @ts-expect-error
+        //   const p = HexagonalGrid.offsetToPixels(
+        //     // @ts-expect-error
+        //     HexagonalGrid.cubeToOffset(c, canvas.grid.grid.options),
+        //     // @ts-expect-error
+        //     canvas.grid.grid.options
+        //   );
+        //   return { x: p.x + Math.floor(canvas.grid!.grid!.w / 2), y: p.y + Math.floor(canvas.grid!.grid!.h / 2) };
+        // });
       } else if (canvas.grid?.type === CONST.GRID_TYPES.SQUARE) {
         // @ts-expect-error
         for (let i = 0; i < this.document.width; ++i) {
