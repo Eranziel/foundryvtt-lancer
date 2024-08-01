8451a79a42748f7391b6b885a51b99270de14d3b
Eranziel <eranziel@gmail.com>
2024-08-01 12:51:00 -0600

    Scaffolding for DamageTarget

---
+++ src/module/apps/damage/DamageTarget.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { DamageHudTarget } from "./data";
+
+  export let target: DamageHudTarget;
+</script>
+
+<div class="damage-hud-target-card card clipped">
+  {target.target.name}
+</div>
+
+<style lang="scss">
+  .damage-hud-target-card {
+    background-color: var(--dark-gray-color);
+  }
+</style>
