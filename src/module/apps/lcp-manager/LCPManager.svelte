748f691d8827e807264d60d43d414b46d47a3561
Eranziel <eranziel@gmail.com>
2025-02-26 09:51:27 -0600

    Add built in progress bar to LCP manager

--- src/module/apps/lcp-manager/LCPManager.svelte
+++ src/module/apps/lcp-manager/LCPManager.svelte
@@ -16,6 +16,11 @@
   let fileContentSummary: ContentSummary | null = null;
   let hoveredContentSummary: ContentSummary | null = null;
   let aggregateContentSummary: ContentSummary | null = null;
+  let importingLcp: IContentPack | null = null;
+  let importing: boolean = false;
+  let barWidth: number = 0;
+  let importingMany: boolean = false;
+  let secondBarWidth: number = 0;
 
   $: temporarySummary = hoveredContentSummary !== null;
   $: contentSummary = hoveredContentSummary ?? fileContentSummary ?? aggregateContentSummary;
@@ -66,12 +71,19 @@
     if (!cp || !manifest) return;
 
     ui.notifications!.info(`Starting import of ${cp.manifest.name} v${cp.manifest.version}. Please wait.`);
+    importing = true;
+    barWidth = 0;
+    importingLcp = cp;
     updateProgressBar(0, 1);
     console.log(`${lp} Starting import of ${cp.manifest.name} v${cp.manifest.version}.`);
     console.log(`${lp} Parsed content pack:`, cp);
     await importCP(cp, (x, y) => updateProgressBar(x, y));
     updateProgressBar(1, 1);
     console.log(`${lp} Import of ${cp.manifest.name} v${cp.manifest.version} complete.`);
+    importing = false;
+    setTimeout(() => {
+      if (!importing && !importingMany) importingLcp = null;
+    }, 1000);
 
     if (cp.manifest.name === "Lancer Core Book Data" && cp.manifest.author === "Massif Press") {
       await game.settings.set(game.system.id, LANCER.setting_core_data, cp.manifest.version);
@@ -82,16 +94,20 @@
 
   async function importManyLcps(lcps: IContentPack[]) {
     if (!_canImportLcp()) return;
-
-    for (const cp of lcps) {
+    importingMany = true;
+    secondBarWidth = 0;
+    for (const [index, cp] of lcps.entries()) {
       if (!cp) continue;
+      secondBarWidth = Math.min(Math.ceil((index / lcps.length) * 100), 100);
       await importLcp(cp);
     }
+    importingMany = false;
   }
 
   function updateProgressBar(done: number, outOf: number) {
-    let percent = Math.ceil((done / outOf) * 100);
+    const percent = Math.min(Math.ceil((done / outOf) * 100), 100);
     SceneNavigation.displayProgressBar({ label: "Importing...", pct: percent });
+    barWidth = percent;
   }
 
   async function clearCompendiums() {
@@ -100,35 +116,52 @@
   }
 </script>
 
-<div class="lcp-manager flexrow">
-  <!-- TODO: event when clicking a package row to show details -->
-  <LCPTable
-    {lcpData}
-    style="grid-area: massif-content"
-    on:lcpHovered={lcpHovered}
-    on:aggregateSummary={event => (aggregateContentSummary = event.detail)}
-    on:installManyLcps={event => importManyLcps(event.detail)}
-    on:clearCompendiums={clearCompendiums}
-  />
-  <div class="lcp-manager__detail-column">
-    <!-- TODO: event when selecting a new manifest -->
-    <LcpSelector {contentPack} style="grid-area: lcp-selector" on:lcpLoaded={lcpLoaded} />
-    <LcpDetails
-      {contentSummary}
-      {temporarySummary}
-      style="grid-area: lcp-details"
-      on:importLcp={event => importLcp()}
+<div class="lcp-manager">
+  <div class="flexrow" style="flex: 1 1;">
+    <!-- TODO: event when clicking a package row to show details -->
+    <LCPTable
+      {lcpData}
+      on:lcpHovered={lcpHovered}
+      on:aggregateSummary={event => (aggregateContentSummary = event.detail)}
+      on:installManyLcps={event => importManyLcps(event.detail)}
+      on:clearCompendiums={clearCompendiums}
     />
+    <div class="lcp-manager__detail-column">
+      <!-- TODO: event when selecting a new manifest -->
+      <LcpSelector {contentPack} style="grid-area: lcp-selector" on:lcpLoaded={lcpLoaded} />
+      <LcpDetails
+        {contentSummary}
+        {temporarySummary}
+        style="grid-area: lcp-details"
+        on:importLcp={event => importLcp()}
+      />
+    </div>
+  </div>
+  <div class="lcp-manager__progress-area">
+    <div class="lcp-manager__progress">
+      {#if importing || importingMany}
+        <span class="monospace">{`${importingLcp?.manifest.name} v${importingLcp?.manifest.version}`} {barWidth}%</span>
+        <div class="lcp-manager__progress-bar" style="width: {barWidth}%" />
+      {/if}
+      {#if importingMany}
+        <div class="lcp-manager__progress-bar" style="width: {secondBarWidth}%" />
+      {/if}
+    </div>
   </div>
 </div>
 
 <style lang="scss">
   .lcp-manager {
+    position: relative;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
     .lcp-manager__detail-column {
       padding-left: 10px;
       padding-right: 10px;
     }
 
+    // Add a little connector between the hovered table row and the detail column
     :global(.lcp-table .row.has-data:not(.header):hover)::after {
       content: "";
       display: block;
@@ -139,6 +172,43 @@
       max-width: 10px;
       height: calc(100% + 20px);
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 20px));
+    }
+
+    .lcp-manager__progress-area {
+      width: 100%;
+      height: 50px;
+      position: relative;
+      bottom: 0;
+      background-color: var(--background-color);
+    }
+    .lcp-manager__progress {
+      width: 100%;
+      height: 100%;
+      padding: 3px;
+      &:has(.lcp-manager__progress-bar) {
+        border: 1px solid var(--color-border-light-tertiary);
+        border-radius: 5px;
+        background-color: var(--darken-2);
+      }
+      & span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .lcp-manager__progress-bar {
+        height: 100%;
+        background-color: var(--primary-color);
+        border: 1px solid #333;
+        transition: width 0.2s;
+        /* If we're importing multiple LCPs, show two bars each half height */
+        &:not(:last-child) {
+          height: 50%;
+        }
+        & + .lcp-manager__progress-bar {
+          height: 50%;
+        }
+      }
     }
   }
 </style>
