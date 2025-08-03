0540af08575c323e5302f4e8b086997b82d7d4d7
Kai Moschcau <kai@kmoschcau.de>
2025-08-03 14:39:56 +0200

    fix: drag and drop handling
    This expands the types for drag and drop in the system and also adds
    more type guards to related functions.

--- src/module/canvas/drop-status.ts
+++ src/module/canvas/drop-status.ts
@@ -1,16 +1,21 @@
+import { type FoundryDropData } from "../helpers/dragdrop";
 import { LancerItem } from "../item/lancer-item";
 
-export function dropStatusToCanvas(
-  canvas: Canvas,
-  data: { type: string; uuid: string; x: number; y: number }
-): void | false {
-  const type: string | undefined = fromUuidSync(data.uuid)?.type;
-  if (data.type !== "Item" || type !== "status") return;
-  const rect = new PIXI.Rectangle(data.x, data.y, 0, 0);
+export function dropStatusToCanvas(canvas: Canvas, data: FoundryDropData): void | false {
+  if (!("uuid" in data) || !("type" in data) || data.type !== "Item") return;
+
+  const { x, y } = data;
+  if (typeof x !== "number" || typeof y !== "number") return;
+
+  const doc = fromUuidSync(data.uuid);
+  if (!doc || !("type" in doc) || doc.type !== "status") return;
+
+  const rect = new PIXI.Rectangle(x, y, 0, 0);
   const tokens: Set<Token.Implementation> = canvas.tokens!.quadtree!.getObjects(rect, {
-    collisionTest: o => o.t.hitArea!.contains(data.x - o.t.x, data.y - o.t.y),
+    collisionTest: o => o.t.hitArea!.contains(x - o.t.x, y - o.t.y),
   }) as any;
   if (tokens.size !== 1 || !tokens.first()?.actor?.isOwner) return;
+
   LancerItem.fromDropData(data as any).then(i => tokens.first()?.actor?.createEmbeddedDocuments("Item", [i! as any]));
   // If we get here, we've succeeded and don't want other handlers to run
   return false;
