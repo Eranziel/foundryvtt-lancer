0e17b527dbec963648333787716117b230e1fafd
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2026-03-30 18:21:59 -0500

    Upgrage vite config
    Fixes the issues with svelte component styles by adding
    vite-plugin-foundryvtt to work around the vite bug that caused it.
    Additionally, upgrade other vite plugins and remove unnecessary
    placeholder files.

--- src/lancer.mjs
+++
@@ -1,6 +0,0 @@
-// only required for dev
-// in prod, foundry loads index.js, which is compiled by vite/rollup
-// in dev, foundry loads index.js, this file, which loads lancer.ts
-
-window.global = window;
-import * as LANCER from "./lancer.ts";
