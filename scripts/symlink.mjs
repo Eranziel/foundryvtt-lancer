9b428826c10cb8454947ac376b728b300853a55c
Eranziel <eranziel@gmail.com>
2026-05-01 23:11:47 -0600

    Print symlink target

--- scripts/symlink.mjs
+++ scripts/symlink.mjs
@@ -7,11 +7,13 @@ if (process.env.CI) process.exit(0);
 
 const dataPath = child.execSync("npx fvtt --config ./fvttrc.yml configure get dataPath").toString().trim();
 if ((dataPath || "undefined") !== "undefined") {
-  const systemDir = path.resolve(dataPath, "Data", "systems", "lancer");
-  try {
-    fs.symlinkSync(path.resolve("dist"), systemDir);
-  } catch (e) {
-    if (e.code === "ENOENT") console.log(`Foundry systemdata dir missing: ${path.normalize(path.join(e.dest, ".."))}`);
-    if (e.code === "EEXIST") console.log("System directory or symlink already exists");
-  }
+    const systemDir = path.resolve(dataPath, "Data", "systems", "lancer");
+    console.log(`Symlinking ${systemDir} to dev build`);
+    try {
+        fs.symlinkSync(path.resolve("dist"), systemDir);
+    } catch (e) {
+        if (e.code === "ENOENT")
+            console.log(`Foundry systemdata dir missing: ${path.normalize(path.join(e.dest, ".."))}`);
+        if (e.code === "EEXIST") console.log("System directory or symlink already exists");
+    }
 }
