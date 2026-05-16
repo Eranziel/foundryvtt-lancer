115a7e6035aa807f8a87642d5eb3a02ab9e69f56
Eranziel <eranziel@gmail.com>
2026-05-16 14:59:26 -0600

    Clean up compcon.ts after merge

--- src/module/util/compcon.ts
+++ src/module/util/compcon.ts
@@ -1,9 +1,4 @@
-import { PackedPilotData } from "./unpacking/packed-types";
-
-// we only cache the id, cloud ids, and name; we're going to fetch all other data on user input
-// the point of the cache is not have the pilot actor window to wait for network calls
-// this does mean that the GM needs to refresh foundry to clear this cache if they add pilots
-// (they could also re-login, we initiate a cache refresh there)
+import type { PackedPilotData } from "./unpacking/packed-types";
 
 export async function fetchPilotViaShareCode(sharecode: string): Promise<PackedPilotData> {
   const shareCodeResponse = await fetch("https://api.compcon.app/share?code=" + sharecode, {
